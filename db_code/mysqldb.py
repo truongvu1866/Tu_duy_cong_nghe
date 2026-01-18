298612329d382ac7977d38e2ddbf0244ce098ee9
truongvu1866 <Truong.VV2416630@sis.hust.edu.vn>
2026-01-18 20:20:35 +0700

    2.0.0

--- db_code/mysqldb.py
+++ db_code/mysqldb.py
@@ -1,4 +1,5 @@
-import mysql.connector
+import mysql
+import mysql.connector as conn
 from mysql.connector import pooling
 
 # Tạo một Pool duy nhất cho toàn ứng dụng
@@ -20,29 +21,58 @@ def get_connection():
     # Lấy một kết nối có sẵn từ pool (Cực nhanh)
     return connection_pool.get_connection()
 
-def insert_box_user(user_id, box_number):
-    conn = get_connection()
-    cursor = conn.cursor()
+def insert_box_user(user_id):
+    insert_result = False
+    connection = get_connection()
+    cursor = connection.cursor()
+    try:
+        query_data = "SELECT * FROM boxes_users WHERE user_id = %s"
+        cursor.execute(query_data, (user_id,))
+        data = cursor.fetchone()
+    except mysql.connector.Error as e:
+        print("Xuất hiện lỗi khi truy vấn dữ liệu" + e)
+        data =None
+    try:
+        if data is None:
+            insert_result = True
+            query_box_id = "SELECT box_id FROM queue WHERE id = '1'"
+            cursor.execute(query_box_id)
+            result_query_box_id = cursor.fetchone()
+            if result_query_box_id:
+                box_id = int(result_query_box_id[0])
+                query_insert = "INSERT INTO boxes_users (user_id, box_number) VALUES (%s, %s)"
+                cursor.execute(query_insert, (user_id,box_id))
+                connection.commit()
+                delete_box_id = "DELETE FROM queue WHERE id = '1'"
+                cursor.execute(delete_box_id)
+                connection.commit()
+    except mysql.connector.Error as err:
+        print("Lỗi khi thêm người mới" + err)
+    finally:
+        cursor.close()
+        connection.close()
+        return insert_result
 
-    cursor.execute(
-        "INSERT INTO boxes_users (user_id, box_number) VALUES (%s, %s)",
-        (user_id, box_number)
-    )
-    conn.commit()
-    cursor.close()
-    conn.close()
-
-def get_box_user(user_id):
-    conn = get_connection()
-    cur = conn.cursor(dictionary=True)
-
-    cur.execute(
-        "SELECT user_id, box_number, time_in FROM boxes_users WHERE user_id = %s",
-        (user_id,)
-    )
-
-    row = cur.fetchone()
-
-    cur.close()
-    conn.close()
-    return row
+def delete_box_user(user_id):
+    delete_result = False
+    connection = get_connection()
+    cursor = connection.cursor()
+    try:
+        query_data ="SELECT box_number FROM boxes_users WHERE user_id = %s"
+        cursor.execute(query_data, (user_id,))
+        result_query = cursor.fetchone()
+        if result_query:
+            delete_result = True
+            box_target = result_query[0]
+            insert_box = "INSERT INTO queue (box_id) VALUES (%s)"
+            cursor.execute(insert_box, (box_target,))
+            connection.commit()
+            query_delete = "DELETE FROM boxes_users WHERE user_id = %s"
+            cursor.execute(query_delete, (user_id,))
+            connection.commit()
+    except mysql.connector.Error as err:
+        print(err)
+    finally:
+        cursor.close()
+        connection.close()
+        return delete_result
--- process/camera_thread.py
+++ process/camera_thread.py
@@ -1,17 +1,39 @@
 import os, cv2, numpy as np
-from queue import Queue
 import time
+
+from queue import Queue
 from PyQt6.QtCore import QThread, pyqtSignal, QMutex, QWaitCondition
 from ultralytics import YOLO
 
-from db_code.mysqldb import insert_box_user, get_box_user, get_connection
 from .face_align import align_face
 from db_code.faissdb import FaceDatabase
+
+import mysql
+import mysql.connector as conn
+
+db_config = {
+    "host": "localhost",
+    "user": "root",
+    "password": "Truong@1866",
+    "database": "face_recognition",
+    "charset": "utf8"
+}
+
+connection_pool = mysql.connector.pooling.MySQLConnectionPool(
+    pool_name="mypool",
+    pool_size=5,
+    **db_config
+)
+
+def get_connection():
+    # Lấy một kết nối có sẵn từ pool (Cực nhanh)
+    return connection_pool.get_connection()
 
 class CameraThread(QThread):
     notification = pyqtSignal(str)
     frame_ready = pyqtSignal(np.ndarray)
     step_signal = pyqtSignal(int)
+    delete_done_signal = pyqtSignal(bool, str)
 
     def __init__(self, db: FaceDatabase, mode, in_mode):
         super().__init__()
@@ -35,6 +57,7 @@ class CameraThread(QThread):
         SFACE_PATH = os.path.join(MODELS_DIR, "face_recognition_sface_2021dec.onnx")
 
         self.embedder = cv2.FaceRecognizerSF.create(SFACE_PATH, "")
+        self.delete_queue = Queue()
 
         self.model = YOLO(YOLO_PATH)
         self.size_process = 416
@@ -42,17 +65,13 @@ class CameraThread(QThread):
         self.THRESHOLD = 0.6
 
         self.db = db
-        self.queue = Queue(10)
-        i = 0
-        while i < 10:
-            self.queue.put(i)
-            i += 1
 
         self.in_mode = in_mode
         self.mode = mode
         self.current_frame = None
         self.is_waiting_next = False
         self.unknown_counter = 0
+        self.user_id_to_delete = None  # Biến tạm lưu ID cần xóa
 
         self.detect_interval = 13
         self.running = True
@@ -65,13 +84,19 @@ class CameraThread(QThread):
         self.frame_count = 0
         self.fps_update_interval = 0.5  # giây
 
+    def stop(self):
+        self.running = False
+        self.condition.wakeAll()
+        if self.cap.isOpened():
+            self.cap.release()
+
     def run(self):
+        self.load_and_process_db_queue()
         while self.running:
             ret, frame = self.cap.read()
             if not ret:break
             if self.mode == "REAL_TIME" :
                 self.frame_id += 1
-
                 if self.frame_id % self.detect_interval == 0:
                     try:
                         self.last_results = self.process(frame)
@@ -85,7 +110,6 @@ class CameraThread(QThread):
                     self.draw_fps(frame)
                 except:
                     print("An error when drawing frame")
-
                 self.frame_ready.emit(frame)
             elif self.mode == "EACH":
                 self.each_process()
@@ -113,38 +137,29 @@ class CameraThread(QThread):
 
             # --- STEP 1: Detect ---
             if not self.wait_for_user(1):return
-            self.notification.emit(f"Bắt đầu detect ảnh")
             face_box = self.detect_face(frame)
-            self.notification.emit(f"Đã hoàn thành detect ảnh")
 
             # --- STEP 2: Embedding ---
             if not self.wait_for_user(2): return
-            self.notification.emit(f"Bắt đầu embedding mặt")
             vector = self.get_embedding(face_box)
-            self.notification.emit(f"Hoàn thành embedding")
 
             # --- STEP 3: Query Database ---
             if not self.wait_for_user(3):return
-            self.notification.emit(f"Bắt đầu truy vấn cơ sở dữ liệu")
             self.last_results = self.query_db(vector)
-            self.notification.emit(f"Đã hoàn thành truy vấn cơ sở dữ liệu")
             for x1, y1, x2, y2, user_id, score in self.last_results:
                 self.notification.emit(f"user_id: {user_id}")
 
             # --- STEP 4: Draw Result ---
             if not self.wait_for_user(4):return
             if self.running and self.current_frame is not None:
-                self.notification.emit(f"Bắt dầu vẽ kết quả")
                 final_img = self.draw_result(self.current_frame)
                 self.frame_ready.emit(final_img)
-            self.notification.emit(f"Quá trình hoàn tất")
             if not self.wait_for_user(4): return
         except Exception as e:
             print("Lỗi hệ thống: {str(e)}")
 
     def process(self, image):
         h, w = image.shape[:2]
-
         img = cv2.resize(image, (self.size_process, self.size_process))
         r = self.model(
             img,
@@ -152,26 +167,19 @@ class CameraThread(QThread):
             device="cpu",
             verbose=False
         )[0]
-
         if r.boxes is None:
             return np.empty((0, 128), dtype=np.float32)
-
         sx, sy = w / self.size_process, h / self.size_process
         boxes = r.boxes.xyxy.cpu().numpy()
         kpts = r.keypoints.xy.cpu().numpy()
-
         results= []
-
         for box, lm in zip(boxes, kpts):
             x1, y1, x2, y2 = (box * [sx, sy, sx, sy]).astype(int)
-
             face = image[y1:y2, x1:x2]
             if face.size == 0:
                 continue
-
             lm = [(int(x * sx), int(y * sy)) for x, y in lm]
             lm_local = [(x - x1, y - y1) for x, y in lm]
-
             aligned = align_face(face, lm_local)
             if aligned is None:
                 continue
@@ -179,41 +187,19 @@ class CameraThread(QThread):
             flatten = emb.flatten()  # Flatten để đảm bảo là vector 1 chiều
             user_id, score = self.db.search(flatten)
             if user_id != "Unknown":
-                if self.unknown_counter != 0:
-                    self.unknown_counter = 0
+                self.unknown_counter = 0
                 if user_id != self.last_id:
-                    self.notification.emit(f"Xin chào {user_id}")
                     self.last_id = user_id
-                    self.notification.emit(f"in mode: {self.in_mode}")
-                    user_data = get_box_user(user_id)
-                    if self.in_mode:
-                        if user_data is not None:
-                            self.notification.emit(f"{user_id} đã có tủ chứa số {user_data['box_number']}")
-                        else:
-                            box_number = self.queue.get()
-                            insert_box_user(user_id, box_number)
-                            self.notification.emit(f"Tủ {box_number} đã mở cho {user_id}")
-                    else:
-                        if user_data is not None:
-                            self.notification.emit(f"Tủ {user_data['box_number']} đã được mở cho {user_id}")
-                            self.queue.put(user_data['box_number'])
-                            try:
-                                self.delete_user(user_id)
-                            except Exception as e:
-                                print(e)
-                        else:
-                            self.notification.emit(f"Không tìm thấy thông tin trùng khớp !")
+                    self.query_mysql_db(self.last_id)
             else:
                 self.unknown_counter += 1
             results.append((x1, y1, x2, y2, user_id, score))
-
         return results
 
     def update_fps(self):
         self.frame_count += 1
         now = time.time()
         elapsed = now - self.prev_time
-
         if elapsed >= self.fps_update_interval:
             self.fps = self.frame_count / elapsed
             self.frame_count = 0
@@ -238,13 +224,6 @@ class CameraThread(QThread):
             2
         )
 
-    def stop(self):
-        self.notification.emit(f"queue have {self.queue.qsize()} items")
-        self.running = False
-        self.condition.wakeAll()
-        if self.cap.isOpened():
-            self.cap.release()
-
     def next_step(self):
         if self.is_waiting_next:
             self.is_waiting_next = False
@@ -263,7 +242,6 @@ class CameraThread(QThread):
 
     def detect_face(self, image):
         h, w = image.shape[:2]
-
         img = cv2.resize(image, (self.size_process, self.size_process))
         r = self.model(
             img,
@@ -271,26 +249,19 @@ class CameraThread(QThread):
             device="cpu",
             verbose=False
         )[0]
-
         if r.boxes is None:
             return np.empty((0, 128), dtype=np.float32)
-
         sx, sy = w / self.size_process, h / self.size_process
         boxes = r.boxes.xyxy.cpu().numpy()
         kpts = r.keypoints.xy.cpu().numpy()
-
         results = []
-
         for box, lm in zip(boxes, kpts):
             x1, y1, x2, y2 = (box * [sx, sy, sx, sy]).astype(int)
-
             face = image[y1:y2, x1:x2]
             if face.size == 0:
                 continue
-
             lm = [(int(x * sx), int(y * sy)) for x, y in lm]
             lm_local = [(x - x1, y - y1) for x, y in lm]
-
             aligned = align_face(face, lm_local)
             results.append((x1, y1, x2, y2, aligned))
         return results
@@ -309,47 +280,32 @@ class CameraThread(QThread):
             user_id, score = self.db.search(flatten)
             results.append((x1, y1, x2, y2, user_id, score))
             if user_id != "Unknown":
-                if self.unknown_counter != 0:
-                    self.unknown_counter = 0
                 if user_id != self.last_id:
-                    self.notification.emit(f"Xin chào {user_id}")
+                    self.unknown_counter = 0
                     self.last_id = user_id
-                    self.notification.emit(f"in mode: {self.in_mode}")
-                    user_data = get_box_user(user_id)
-                    if self.in_mode:
-                        if user_data is not None:
-                            self.notification.emit(f"{user_id} đã có tủ chứa số {user_data['box_number']}")
-                        else:
-                            box_number = self.queue.get()
-                            insert_box_user(user_id, box_number)
-                            self.notification.emit(f"Tủ {box_number} đã mở cho {user_id}")
-                    else:
-                        if user_data is not None:
-                            self.notification.emit(f"Tủ {user_data['box_number']} đã được mở cho {user_id}")
-                            self.queue.put(user_data['box_number'])
-                            try:
-                                self.delete_user(user_id)
-                            except:
-                                print("can not delete box user")
-                        else:
-                            self.notification.emit(f"Không tìm thấy thông tin trùng khớp !")
+                    try:
+                        self.query_mysql_db(self.last_id)
+                    except mysql.connector.Error as err:
+                        print("Lỗi khi chạy truy xuất mysql database" + str(err))
             else:
-                self.unknown_counter += 1
+                self.unknown_counter = +1
         return results
+
+    def query_mysql_db(self, user_id):
+        if self.in_mode:
+            self.insert_box_user(user_id)
+        else:
+            self.delete_box_user(user_id)
 
     def draw_result(self, image):
         if image is None or len(self.last_results) == 0:
             return image
-
-        # Tạo một bản sao độc lập để tránh xung đột vùng nhớ
         annotated_image = image.copy()
-
         try:
             for x1, y1, x2, y2, user_id, score in self.last_results:
                 # Ép kiểu tọa độ về int để OpenCV không lỗi
                 p1 = (int(x1), int(y1))
                 p2 = (int(x2), int(y2))
-
                 cv2.rectangle(annotated_image, p1, p2, (0, 255, 0), 2)
                 label = f"{user_id}: {score:.2f}"
                 cv2.putText(annotated_image, label, (p1[0], p1[1] - 10),
@@ -359,16 +315,82 @@ class CameraThread(QThread):
             print(f"Lỗi vẽ hình: {e}")
             return image
 
-    def delete_user(self,user_id):
-        conn = get_connection()  # Lấy từ pool
-        cursor = conn.cursor()
+    '''MYSQL DATABASE QUERY FORM HERE'''
+
+    def load_and_process_db_queue(self):
+        conn = get_connection()
+        cursor = conn.cursor(dictionary=True)
         try:
-            cursor.execute("DELETE FROM boxes_users WHERE id = %s",
-            (user_id,)
-            )
+              # Dùng dictionary để dễ truy cập theo tên cột
+            query = "SELECT id, box_id FROM delete_queue WHERE status = 'In' ORDER BY id ASC"
+            cursor.execute(query)
+            rows = cursor.fetchall()
+            if not rows:
+                self.notification.emit("Không có dữ liệu cũ trong hàng đợi DB.")
+                return
+            count = 0
+            for row in rows:
+                db_id = row['id']
+                box_id = row['box_id']
+                self.delete_queue.put(box_id)
+                count += 1
             conn.commit()
+            self.notification.emit(f"Đã nạp lại {count} yêu cầu từ Database.")
+        except mysql.connector.Error as err:
+            self.notification.emit(f"Lỗi DB khi nạp hàng đợi: {err}")
         finally:
-            # Quan trọng: Đóng cursor và conn ở đây không phải là ngắt kết nối,
-            # mà là trả kết nối này về pool để người khác dùng.
+            if conn and conn.is_connected():
+                cursor.close()
+                conn.close()
+
+    def insert_box_user(self, user_id):
+        connection = get_connection()
+        cursor = connection.cursor()
+        try:
+            query_data = "SELECT * FROM boxes_users WHERE user_id = %s"
+            cursor.execute(query_data, (user_id,))
+            data = cursor.fetchone()
+        except mysql.connector.Error as e:
+            print("Xuất hiện lỗi khi truy vấn dữ liệu" + str(e))
+            data = None
+        try:
+            if data is None:
+                target_box_id  = self.delete_queue.get()
+                if target_box_id:
+                    update_queue = "UPDATE delete_queue SET status = %s WHERE box_id = %s"
+                    cursor.execute(update_queue, ('Not_in', target_box_id))
+                    connection.commit()
+                    insert_new_user = "INSERT INTO boxes_users (user_id, box_number) VALUES (%s, %s)"
+                    cursor.execute(insert_new_user, (user_id,target_box_id))
+                    connection.commit()
+                    self.notification.emit(f"{user_id} check in thành công")
+                    self.notification.emit(f"{user_id}: tủ {target_box_id}")
+        except mysql.connector.Error as err:
+            print("Lỗi khi thêm người mới " + str(err))
+        finally:
             cursor.close()
-            conn.close()
+            connection.close()
+
+    def delete_box_user(self, user_id):
+        connection = get_connection()
+        cursor = connection.cursor()
+        try:
+            query_data = "SELECT box_number FROM boxes_users WHERE user_id = %s"
+            cursor.execute(query_data, (user_id,))
+            result_query = cursor.fetchone()
+            if result_query:
+                box_target = result_query[0]
+                update_queue = "UPDATE delete_queue SET status = %s WHERE box_id = %s"
+                cursor.execute(update_queue, ('In', box_target))
+                connection.commit()
+                self.delete_queue.put(box_target)
+                query_delete = "DELETE FROM boxes_users WHERE user_id = %s"
+                cursor.execute(query_delete, (user_id,))
+                connection.commit()
+                self.notification.emit(f"{user_id} check out thành công")
+                self.notification.emit(f"{user_id}: tủ {box_target}")
+        except mysql.connector.Error as err:
+            print(err)
+        finally:
+            cursor.close()
+            connection.close()
